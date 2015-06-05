0890c27ee5fe6a8c1b21f4cfcd1050de949f37d7
andrej ocenas <mr.ocenas@gmail.com>
2015-06-05 23:20:46 +0200

    tying to do simplest test but not working

---
+++ tests/TodoTest.js
@@ -0,0 +1,15 @@
+jest.dontMock('../js/Todo');
+
+describe('is immutable', function() {
+    it('returns new Todo on complete()', function() {
+        console.log('testing');
+        var Todo = require('../js/Todo');
+        //var todo = Todo('test');
+        //expect(todo.completed).toEqual(false);
+
+        //var newTodo = todo.complete();
+        //expect(newTodo.completed).toEqual(true);
+        //expect(todo.completed).toEqual(false);
+
+    });
+});
